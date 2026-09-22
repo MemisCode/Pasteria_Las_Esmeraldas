cdf4aa806e4455f1e3f6ebcfee501c234d13b75d
Victor Rocha <vjrc1980@gmail.com>
2026-09-21 21:41:52 -0600

    Agregados estilos CSS y funcion de WhatsApp

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function pedirPorWhatsApp(nombreProducto) {
-  // Ingresa tu número telefónico con el código de país (ejemplo: 506 para Costa Rica)
-  const telefono = "50688888888"; 
-  const mensaje = encodeURIComponent(`¡Hola! Quisiera consultar sobre el producto: ${nombreProducto} en Pastelería Las Esmeraldas.`);
+  // Reemplaza con tu número telefónico con el código de país (ejemplo: 506 para Costa Rica)
+  const telefono = "50600000000"; 
+  const mensaje = encodeURIComponent(`¡Hola! Quisiera consultar/pedir el producto: "${nombreProducto}" en Pastelería Las Esmeraldas.`);
   
   window.open(`https://wa.me/${telefono}?text=${mensaje}`, '_blank');
 }
